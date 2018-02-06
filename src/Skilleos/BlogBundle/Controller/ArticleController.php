b07db7f57acbd90f099db968d18d9879832b8c58
paudru <pauline.druesne@edu.ece.fr>
2018-02-06 23:25:56 +0100

    add + edit + view (sans style)

--- src/Skilleos/BlogBundle/Controller/ArticleController.php
+++ src/Skilleos/BlogBundle/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 namespace Skilleos\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Skilleos\BlogBundle\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends Controller
 {
@@ -13,17 +15,60 @@ class ArticleController extends Controller
 
     public function viewAction($id)
     {
-    	return $this->render('SkilleosBlogBundle:Article:view.html.twig', array('id' => $id));
+    	$article = $this->getDoctrine()->getManager()->getRepository('SkilleosBlogBundle:Article')->find($id);
+
+    	return $this->render('SkilleosBlogBundle:Article:view.html.twig', array('article' => $article));
     }
 
-    public function addAction()
+    public function addAction(Request $request)
     {
-        return $this->render('SkilleosBlogBundle:Article:add.html.twig');
+        $article = new Article();
+
+        $form = $this->get('form.factory')
+          ->createBuilder('form', $article)
+          ->add('title', 'text')
+          ->add('content', 'textarea')
+          ->add('add', 'submit')
+          ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()){
+        	$em = $this->getDoctrine()->getManager();
+        	$em->persist($article);
+        	$em->flush();
+
+        return $this->redirect($this->generateUrl('skilleos_blog_view', array('id' => $article->getId())));
+        }
+
+        return $this->render('SkilleosBlogBundle:Article:add.html.twig', array('form' => $form->createView()));
     }
 
-    public function editAction($id)
+    public function editAction($id, Request $request)
     {
-        return $this->render('SkilleosBlogBundle:Article:edit.html.twig', array('id' => $id));
+        $article = $this->getDoctrine()
+          ->getManager()
+          ->getRepository('SkilleosBlogBundle:Article')
+          ->find($id);
+
+           $form = $this->get('form.factory')
+          ->createBuilder('form', $article)
+          ->add('title', 'text')
+          ->add('content', 'textarea')
+          ->add('edit', 'submit')
+          ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()){
+        	$em = $this->getDoctrine()->getManager();
+        	$em->persist($article);
+        	$em->flush();
+        }
+
+
+
+        return $this->render('SkilleosBlogBundle:Article:edit.html.twig', array('form' => $form->createView()));
     }
 
     public function deleteAction()
